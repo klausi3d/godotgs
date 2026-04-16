59f3dadeaf90ed6ac656a235147a6d2d5a0ca3ba
Alexander Klaus <alexander-klaus@outlook.de>
2026-04-16 23:09:20 +0200

    perf: hoist cache invariants in gaussian animation apply loop

--- modules/gaussian_splatting/core/gaussian_data_animation.cpp
+++ modules/gaussian_splatting/core/gaussian_data_animation.cpp
@@ -61,21 +61,22 @@ void GaussianData::apply_animation_at_time(float p_time) {
     }
     RWLockWrite lock(data_rwlock);
     MutexLock anim_lock(animation_cache_mutex);
+    const uint32_t gaussian_count = gaussians.size();
 
     bool animate_positions = animation_state_machine->has_active_track(GaussianSplatting::ANIMATION_PROPERTY_POSITION);
     bool animate_colors = animation_state_machine->has_active_track(GaussianSplatting::ANIMATION_PROPERTY_COLOR);
     bool animate_opacities = animation_state_machine->has_active_track(GaussianSplatting::ANIMATION_PROPERTY_OPACITY);
 
-    bool needs_position_cache = animate_positions && animated_positions_cache.size() != gaussians.size();
-    bool needs_color_cache = animate_colors && animated_colors_cache.size() != gaussians.size();
-    bool needs_opacity_cache = animate_opacities && animated_opacities_cache.size() != gaussians.size();
+    bool needs_position_cache = animate_positions && animated_positions_cache.size() != gaussian_count;
+    bool needs_color_cache = animate_colors && animated_colors_cache.size() != gaussian_count;
+    bool needs_opacity_cache = animate_opacities && animated_opacities_cache.size() != gaussian_count;
 
     // Update cache if needed
     if (animation_cache_dirty || p_time != last_animation_time || needs_position_cache || needs_color_cache || needs_opacity_cache) {
         if (animate_positions) {
-            animated_positions_cache.resize(gaussians.size());
-            animated_positions_valid_cache.resize(gaussians.size());
-            for (uint32_t i = 0; i < gaussians.size(); i++) {
+            animated_positions_cache.resize(gaussian_count);
+            animated_positions_valid_cache.resize(gaussian_count);
+            for (uint32_t i = 0; i < gaussian_count; i++) {
                 Vector3 sampled_position;
                 if (animation_state_machine->try_sample_position(i, p_time, sampled_position)) {
                     animated_positions_cache[i] = sampled_position;
@@ -90,9 +91,9 @@ void GaussianData::apply_animation_at_time(float p_time) {
         }
 
         if (animate_colors) {
-            animated_colors_cache.resize(gaussians.size());
-            animated_colors_valid_cache.resize(gaussians.size());
-            for (uint32_t i = 0; i < gaussians.size(); i++) {
+            animated_colors_cache.resize(gaussian_count);
+            animated_colors_valid_cache.resize(gaussian_count);
+            for (uint32_t i = 0; i < gaussian_count; i++) {
                 Color sampled_color;
                 if (animation_state_machine->try_sample_color(i, p_time, sampled_color)) {
                     animated_colors_cache[i] = sampled_color;
@@ -107,9 +108,9 @@ void GaussianData::apply_animation_at_time(float p_time) {
         }
 
         if (animate_opacities) {
-            animated_opacities_cache.resize(gaussians.size());
-            animated_opacities_valid_cache.resize(gaussians.size());
-            for (uint32_t i = 0; i < gaussians.size(); i++) {
+            animated_opacities_cache.resize(gaussian_count);
+            animated_opacities_valid_cache.resize(gaussian_count);
+            for (uint32_t i = 0; i < gaussian_count; i++) {
                 float sampled_opacity;
                 if (animation_state_machine->try_sample_opacity(i, p_time, sampled_opacity)) {
                     animated_opacities_cache[i] = sampled_opacity;
@@ -141,17 +142,20 @@ void GaussianData::apply_animation_at_time(float p_time) {
     }
 
     // Apply animated values to gaussians
-    for (uint32_t i = 0; i < gaussians.size(); i++) {
+    const bool can_apply_positions = animate_positions && animated_positions_cache.size() == gaussian_count && animated_positions_valid_cache.size() == gaussian_count;
+    const bool can_apply_colors = animate_colors && animated_colors_cache.size() == gaussian_count && animated_colors_valid_cache.size() == gaussian_count;
+    const bool can_apply_opacities = animate_opacities && animated_opacities_cache.size() == gaussian_count && animated_opacities_valid_cache.size() == gaussian_count;
+    for (uint32_t i = 0; i < gaussian_count; i++) {
         // Check if there are animated values for this splat
-        if (animate_positions && i < animated_positions_cache.size() && i < animated_positions_valid_cache.size() && animated_positions_valid_cache[i]) {
+        if (can_apply_positions && animated_positions_valid_cache[i]) {
             gaussians[i].position = animated_positions_cache[i];
         }
 
-        if (animate_colors && i < animated_colors_cache.size() && i < animated_colors_valid_cache.size() && animated_colors_valid_cache[i]) {
+        if (can_apply_colors && animated_colors_valid_cache[i]) {
             gaussians[i].sh_dc = animated_colors_cache[i];
         }
 
-        if (animate_opacities && i < animated_opacities_cache.size() && i < animated_opacities_valid_cache.size() && animated_opacities_valid_cache[i]) {
+        if (can_apply_opacities && animated_opacities_valid_cache[i]) {
             gaussians[i].opacity = animated_opacities_cache[i];
         }
     }
